f6c825e8b91f6fe2f21835a9550cbd8ffc4be399
Bruno <brunoascenc1@outlook.com>
2020-07-28 13:12:49 -0300

    genres

--- js/app.js
+++ js/app.js
@@ -2,14 +2,14 @@ const searchButton = document.getElementById("search");
 const inputElement = document.getElementById("searchInput");
 const movieSearch = document.getElementById("movies-search");
 const moviesContainer = document.getElementById("movie-container");
-const movieReviews = document.querySelector(".movie-reviews");
+// const movieReviews = document.querySelector(".movie-reviews");
 const similarMovies = document.getElementById("similar-movies");
-const detailsContainer = document.getElementById("movie-detail");
+// const detailsContainer = document.getElementById("movie-detail");
 
 //movie items
 const movieSection = (movie) => {
   const section = document.createElement("section");
-  section.classList = "section";
+  section.classList = "movie-section";
 
   movie.map((movie) => {
     if (movie.poster_path) {
@@ -18,15 +18,14 @@ const movieSection = (movie) => {
       img.setAttribute("data-movie-id", movie.id);
       img.addEventListener("click", function () {
         movieDetails(movie.id);
-        showDetails();
+        // showDetails();
       });
 
       section.appendChild(img);
     }
   });
-
   return section;
-};
+}
 
 //create movie container
 const movieContaier = (movies, title = "") => {
@@ -60,6 +59,7 @@ function renderMovies(data) {
   moviesContainer.appendChild(movieBlock);
 }
 
+//Error
 const handleError = (error) => {
   console.log(error);
 };
@@ -73,66 +73,59 @@ searchButton.addEventListener("click", (e) => {
   inputElement.value = "";
 });
 
-//movie id
+//get movies id
 function movieDetails(id) {
   sessionStorage.setItem("movieId", id);
   window.location = "movie.html";
   return false;
 }
 
-//Movie Details
+
+//Movie Details output
 const getDetails = (data) => {
   const movie = data;
   const genre = data.genres;
+  let output2 = ``
+  genre.map(genres => {
+    console.log(genres.name)
+    output2 =`
+    <li>Genres: ${genres.name}</li>
+    `
 
-  let output = `
-            <img src = "${IMAGE_URL + movie.poster_path}"  data-movie-id="${movie.id}"/>
-            <div>
-              <h1>Movie overview</h1>
-              <p>${movie.overview}</p>
-            </div>
-            <div class = "detailInfo">
-            <h1>Details</h1>
-               <ul>
-                  <li>Title: ${movie.title}</li>
-                  <li>Original Title: ${movie.original_title}</li>
-                  <li>Release Date: ${movie.release_date}</li>
-                  <li>Genre: ${genre[0].name}</li>
-                  <li>Duration: ${movie.runtime}</li>
-                  <li>Rating: ${movie.vote_average}</li>
-               </ul>
-            </div>
-            <button class= "trailerBtn">Trailer</button>
-            `;
+    document.getElementById("movie-detail2").innerHTML = output2;
+  })
+  // console.log(genre)
+
+  // let output = ` `
+  // for (let i in genre){
+    let output = `
+    <img src = "${IMAGE_URL + movie.poster_path}"  data-movie-id="${movie.id}"/>
+    <div>
+      <h1>Movie overview</h1>
+      <p>${movie.overview}</p>
+    </div>
+    <div class = "detailInfo">
+    <h1>Details</h1>
+       <ul>
+          <li>Title: ${movie.title}</li>
+          <li>Original Title: ${movie.original_title}</li>
+          <li>Release Date: ${movie.release_date}</li>
+          <li>Genre: ${genre[0].name}</li>
+          <li>Duration: ${movie.runtime}</li>
+          <li>Rating: ${movie.vote_average}</li>
+       </ul>
+    </div>
+    <button class= "trailerBtn">Trailer</button>
+
+    `
+  // }
+          
   document.getElementById("movie-detail").innerHTML = output;
+  // document.getElementById("movie-detail").innerHTML = outpu2;
+
 };
 
-// // Movie Trailer
-// const getTrailer = (data) => {
-//   const movie = data.results
-//   // console.log(movie)
-//   document.addEventListener('click', e => {
-//     if (e.target.className === 'trailerBtn'){
-//       // let movieId = sessionStorage.getItem("movieId");
-//       // const path = `/movie/${movieId}/videos`;
-//       // const url = dinamicUrl(path);
-
-//       // fetch(url)
-//       //   .then((res) => res.json())
-//       //   .then((data) => {
-//       //      const movie = data.results;
-//       //      console.log(movie);
-
-//            let output = `
-//               <iframe src = "https://www.youtube.com/embed/${movie[0].key}" width="250" height="300"></iframe>
-//            `;
-//            document.querySelector(".movie-trailer").innerHTML = output;
-//         // });
-//     }
-//   })
-// }
-
-// Movie Trailer
+// Display movie trailer
 const getTrailer = (data) => {
   const movie = data.results;
   document.addEventListener("click", e => {
@@ -146,18 +139,19 @@ const getTrailer = (data) => {
 };
 
 //Movie Reviews
-const getReviews = (data) => {
+function getReviews(data) {
   const movie = data.results;
   movie.length = 4;
   let output = `<h2>Reviews</h2>`;
   for (let i in movie) {
     output += `
-            <h3>${movie[i].author}</h3>
-            <p>${movie[i].content}</p>
-        `;
+        <h3>${movie[i].author}</h3>
+        <p>${movie[i].content}</p>
+        <a href = "${movie[i].url}">Link</a>
+    `;
   }
   document.querySelector(".movie-reviews").innerHTML = output;
-};
+}
 
 //Similar movies
 function getSimilarMovies(data) {
@@ -167,15 +161,16 @@ function getSimilarMovies(data) {
   similarMovies.appendChild(movieBlock);
 }
 
-const showDetails = () => {
-  document.addEventListener("click", (e) => {
-    const target = e.target;
-    if (target.tagName === "IMG") {
-      var movieId = target.getAttribute("data-movie-id");
-      movieDetails(movieId);
-    }
-  });
-};
+// Display movie details
+// const showDetails = () => {
+//   document.addEventListener("click", (e) => {
+//     const target = e.target;
+//     if (target.tagName === "IMG") {
+//       var movieId = target.getAttribute("data-movie-id");
+//       movieDetails(movieId);
+//     }
+//   });
+// };
 
 function movieData() {
   getMovieDetails();
